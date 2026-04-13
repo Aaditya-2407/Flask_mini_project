ec5605e185dbb41a13c84d4b6dd5aa507c97066f
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-04-13 23:10:00 +0530

    step -3

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
-from flask import Flask ,request
+from flask import Flask, request
 from werkzeug.utils import secure_filename
+from extractor import extract_text_from_pdf
 import os
 
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/upload', methods=['POST'])
+@app.route("/")
+def home():
+    return "Flask is working!"
+
+@app.route("/upload", methods=["POST"])
 def upload_pdf():
-    file=request.files.get['pdf']
+    file = request.files.get("pdf")
 
-    if not file or file.filename == '':
-        return "No file uploaded", 400
-    
-    if not file.filename.lower().endswith('.pdf'):
-        return "Only PDF files are allowed", 400
+    if not file or file.filename == "":
+        return {"error": "No PDF uploaded"}, 400
+
+    if not file.filename.endswith(".pdf"):
+        return {"error": "Only PDF files allowed"}, 400
+
     filename = secure_filename(file.filename)
     path = os.path.join(UPLOAD_FOLDER, filename)
     file.save(path)
-    return {"message": "PDF Uploaded", "filename": filename}, 200
 
-@app.route('/')
-def hello_world():
-    
-    return "Flask app is running!       "
+    text = extract_text_from_pdf(path)
+
+    return {
+        "message": "PDF uploaded",
+        "filename": filename,
+        "text": text[:1000]
+    }
