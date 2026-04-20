008cfd8faa204afb69d8c976a6542e86a6d90157
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-04-20 15:09:23 +0530

    Add gunicorn and flask-cors for Render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     
     # Configure upload folder
     app.config['UPLOAD_FOLDER'] = 'uploads'
